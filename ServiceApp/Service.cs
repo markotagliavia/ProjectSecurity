0d491b3d27e6c4f975c35b41ca3f802cb6116f6d
KristijanSalaji <33682513+KristijanSalaji@users.noreply.github.com>
2017-11-15 10:56:39 +0100

    ServiceApp and Contracts init

---
+++ ServiceApp/Service.cs
@@ -0,0 +1,37 @@
+﻿using Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceApp
+{
+    public class Service : IService
+    {
+        public void ChangePassword(string oldPassowrd, string newPassword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogIn(string email, string password, string code)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogOut(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Registration(string name, string sname, DateTime date, char gender, string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ResetPassword(string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
