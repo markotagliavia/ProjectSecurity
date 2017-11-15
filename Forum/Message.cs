8d35a18c44c185713aa56d17a4992525ae6844e5
Tijana994 <tijana.vdn@gmail.com>
2017-11-15 20:22:11 +0100

    Model of class Message

--- Forum/Message.cs
+++ Forum/Message.cs
@@ -8,5 +8,72 @@ namespace Forum
 {
     class Message
     {
+        private string text;
+
+        private Guid code;
+
+        private Guid user;
+
+        private DateTime creationTime;
+
+        public Message(string text, Guid user)
+        {
+            this.Text = text;
+            this.User = user;
+            this.Code = System.Runtime.InteropServices.Marshal.GenerateGuidForType(typeof(int));
+            this.CreationTime = DateTime.Now;
+        }
+
+        public string Text
+        {
+            get
+            {
+                return text;
+            }
+
+            set
+            {
+                text = value;
+            }
+        }
+
+        public Guid Code
+        {
+            get
+            {
+                return code;
+            }
+
+            set
+            {
+                code = value;
+            }
+        }
+
+        public Guid User
+        {
+            get
+            {
+                return user;
+            }
+
+            set
+            {
+                user = value;
+            }
+        }
+
+        public DateTime CreationTime
+        {
+            get
+            {
+                return creationTime;
+            }
+
+            set
+            {
+                creationTime = value;
+            }
+        }
     }
 }
