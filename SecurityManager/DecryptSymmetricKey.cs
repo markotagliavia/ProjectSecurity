e6577d3ad4b4927ef377b85c8d2fcc110bffcccf
markotagliavia <max.tagliavia@gmail.com>
2017-11-20 23:21:02 +0100

    RSA + AES encrypt decrypt files created

---
+++ SecurityManager/DecryptSymmetricKey.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using static SecurityManager.GennerateRsaKey;
+
+namespace SecurityManager
+{
+    class DecryptSymmetricKey
+    {
+
+       /* public void SetSymmetricKey(Guid id, byte[] encryptedKey)
+        {
+            SessionKey session = sessionKeys[id];
+
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+            {
+                rsa.ImportParameters(session.PrivateKey);
+
+                session.SymmetricKey = rsa.Decrypt(encryptedKey, true);
+            }
+        }*/
+    }
+}
