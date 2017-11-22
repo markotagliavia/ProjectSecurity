9490172659e2911a4809f6206a4315778b2fb784
markotagliavia <max.tagliavia@gmail.com>
2017-11-22 20:17:48 +0100

    XML Network configuration file created and used to load IP and port. Unsuccesfull connection to the server handled

--- ServiceApp/Program.cs
+++ ServiceApp/Program.cs
@@ -4,12 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel;
 using System.Text;
-using System.Threading.Tasks;
-using Contracts;
 using SecurityManager;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Runtime.Serialization;
+using System.Xml.Linq;
+using System.Reflection;
+using System.Xml;
 
 namespace ServiceApp
 {
@@ -21,12 +22,26 @@ namespace ServiceApp
             System.Security.Cryptography.SHA256Managed sha256hasher = new System.Security.Cryptography.SHA256Managed();
             byte[] hashedDataBytes = sha256hasher.ComputeHash(encoder.GetBytes(phrase));
             return Convert.ToBase64String(hashedDataBytes);
-        }  // ovo ne treba
+        } 
+
         static void Main(string[] args)
         {
-
+            string ip = "";
+            string port = "";
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load((Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), @"NetworkConfig.xml")).ToString());
+                ip = doc.GetElementsByTagName("IPAddress")[0].InnerXml;
+                port = doc.GetElementsByTagName("Port")[0].InnerXml;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("NetworkConfig file is missing!");
+                Console.ReadLine();
+            }
             NetTcpBinding binding = new NetTcpBinding();
-            string address = "net.tcp://localhost:27019/ServiceApp";
+            string address = $"net.tcp://{ip}:{port}/ServiceApp";
 
             ServiceHost host = new ServiceHost(typeof(Service));
             host.AddServiceEndpoint(typeof(IService), binding, address);
